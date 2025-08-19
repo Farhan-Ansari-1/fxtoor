1a0c2037603f5ccb4fe46697efb7994487e7f65e
farhan-ansari-1 <ansarifarhan172006@gmail.com>
2025-08-19 17:03:12 +0530

    refactor: Improve sitemap and robots.txt generation

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
 
+const base = '/fxtoor/';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
@@ -9,15 +11,23 @@ export default defineConfig(({ command }) => {
       react(),
       sitemap({
         hostname: 'https://Farhan-Ansari-1.github.io',
+        base: base, // Base path alag se batayein
         dynamicRoutes: [
           '/about',
           '/contact',
           '/privacy',
           '/terms'
-        ]
+        ],
+        // robots.txt bhi yahin se generate karein
+        robots: [
+          {
+            userAgent: '*',
+            allow: '/',
+          },
+        ],
       }),
     ],
-    base: command === 'serve' ? '/' : '/fxtoor/', // dev me '/' aur prod me repo name
+    base: command === 'serve' ? '/' : base, // dev me '/' aur prod me repo name
     server: {
       port: command === 'serve' ? 5173 : undefined, // sirf dev me port set hoga
     },
