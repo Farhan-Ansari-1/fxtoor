329a04b8c97f3a288dbb1bccff84b0d7b3160b55
farhan-ansari-1 <ansarifarhan172006@gmail.com>
2026-01-23 01:48:08 +0530

    Set permanent dark mode

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 
 // 1. Context ko create karein
 const ThemeContext = createContext();
